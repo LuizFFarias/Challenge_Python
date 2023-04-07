7cb337e3d508a72301815259dd646a88c6ca9699
user_LuizFFarias <user_rm99519@fiap.com.br>
2023-04-07 03:04:37 -0300

    feedback adicionado e problemas no cadastro encontrados

--- challenge.py
+++ challenge.py
@@ -1,6 +1,8 @@
 cont = 0
 cadastro = 0
 opcao = 0
+diahoje = 0
+meshoje = 0
 nome = 0
 dianascimento = 0
 mesnascimento = 0
@@ -18,45 +20,55 @@ modelo = 0
 valor = 0
 cor = 0
 ntfiscal = 0
+fbtempo = 0
+fbservicos = 0
+fbproblemas = 0
+fbatendimentos = 0
+fbduvidas = 0
 
 print("Olá, em que a Technobike pode te ajudar hoje?")
 while opcao != 5:
-    print("\n1 - cadastro")
-    print("2 - registrar bike")
-    print("3 - feedback")
-    print("4 - contrato")
-    print("5 - encerrar")
-    print("1 - cadastro")
-    print("1 - cadastro")
-    print("1 - cadastro")
+    print("\n1 - Cadastro")
+    print("2 - Registrar bike")
+    print("3 - Feedback")
+    print("4 - Contrato")
+    print("5 - Relatório")
+    print("6 - Encerrar")
     opcao = int(input("\nSelecione uma das opções acima: "))
 
-    if opcao == 5:
+    if opcao == 6:
           print("\nEncerrando, até a próxima!")
 
 #cadastro
-    
-    if opcao == 1:
-       # while cadastro == 0:
+    #while cadastro != 1:    
+    if opcao == 1: 
+        diahoje = int(input("Digite o dia de hoje: "))
+        meshoje = int(input("Digite o mês atual: "))
         nome = input("\nDigite seu nome: ")
         dianascimento = int(input("Digite o dia de seu nascimento: "))
-        mesnascimento = int(input("Digite o mes de seu nascimento: "))
-        anonascimento = int(input("Digite o ano de seu nascimento: "))
-        if dianascimento > 0 and dianascimento < 32 and  mesnascimento > 0 and mesnascimento < 13 and anonascimento < 2006 and anonascimento > 1903:
-            print("\n1 - RG")
-            print("2 - CNH")
-            cdopcao2 = int(input("\nSelecione uma opção: "))
+        if dianascimento > 0 and dianascimento < 32:
+            mesnascimento = int(input("Digite o mes de seu nascimento: "))
+        elif  mesnascimento > 0 and mesnascimento < 13:
+            anonascimento = int(input("Digite o ano de seu nascimento: "))
+        elif anonascimento < 2006 and anonascimento > 1903:
+            print(" ")
+        if meshoje == mesnascimento and anonascimento == 2005 and diahoje > dianascimento:
+                print("\n1 - RG")
+                print("2 - CNH")
+                cdopcao2 = int(input("\nSelecione uma opção: "))
+            
 
-            if cdopcao2 == 1:
-                    rg = input("\nDigite seu RG: ")
-            elif cdopcao2 == 2: 
-                    cnh = int(input("\nDigite sua CNH: "))
-            else:
-                    print("\nDigite um número válido!")
-                    cadastro +=  1
+                if cdopcao2 == 1:
+                        rg = input("\nDigite seu RG: ")
+                elif cdopcao2 == 2: 
+                        cnh = int(input("\nDigite sua CNH: "))
+                else:
+                        print("\nDigite um número válido!")
+                        break
         else:
-                print("Digite uma data válida!")
-                cadastro += 1 
+            print("Digite uma data válida!")
+            cadastro += 1 
+            break
         cpf = int(input("Digite seu CPF: "))
         if cpf > 99999999999 and cpf < 1:
                 print("Digite um número válido!")
@@ -106,16 +118,72 @@ while opcao != 5:
         print(f"Número de série: {numserie}")
         print(f"Nota fiscal: {ntfiscal}")
         print(f"Cor: {cor}")
-        rgopcao = int(input("\n1 - Sim \n2 - Não \nSelecione uma opção: "))
+        print("\n1 - Sim \n2 - Não")
+        rgopcao = int(input("\nSelecione uma opção: "))
         if rgopcao == 1:
             print("\nCadstro realizado.")
         else:
-            print("Faça novamente o cadastro.")
+            print("Faça novamente o registro.")
             
 #feedback
-    #if opcao == 3: 
-         
+    if opcao == 3: 
+        print("\nQual o motivo do feedback? \n1 - Tempo de espera")
+        print("2 - Serviços")
+        print("3 - Problemas")
+        print("4 - Atendimeto")
+        print("5 - Dúvidas")
+        fbopcao = int(input("Selecione uma opção: "))
+        if fbopcao ==1:
+            fbtempo = input("\nEscreva seu feedback: ")
+            print("\nTem certeza que vai enviar o feedback? \n1 - Sim \n2 - Não")
+            fbopcao2 = int(input("\nSelecione uma opção: "))
+            if fbopcao2 == 1:
+                print("Feedback enviado!")
+            elif fbopcao2 == 2:
+                print("Feedbck cancelado!")
+            else:
+                print("Digite uma opção válida!")
+        elif fbopcao == 2:
+            fbservicos = input("\nEscreva seu feedback: ")
+            print("\nTem certeza que vai enviar o feedback? \n1 - Sim \n2 - Não")
+            fbopcao2 = int(input("\nSelecione uma opção: "))
+            if fbopcao2 == 1:
+                print("Feedback enviado!")
+            elif fbopcao2 == 2:
+                print("Feedbck cancelado!")
+            else:
+                print("Digite uma opção válida!")
+        elif fbopcao == 3 :
+            fbproblemas = input("\nEscreva seu feedback: ")
+            print("\nTem certeza que vai enviar o feedback? \n1 - Sim \n2 - Não")
+            fbopcao2 = int(input("\nSelecione uma opção: "))
+            if fbopcao2 == 1:
+                print("Feedback enviado!")
+            elif fbopcao2 == 2:
+                print("Feedbck cancelado!")
+            else:
+                print("Digite uma opção válida!")
+        elif fbservicos == 4:
+            fbatendimentos = input("\nEscreva seu feedback: ")
+            print("\nTem certeza que vai enviar o feedback? \n1 - Sim \n2 - Não")
+            fbopcao2 = int(input("\nSelecione uma opção: "))
+            if fbopcao2 == 1:
+                print("Feedback enviado!")
+            elif fbopcao2 == 2:
+                print("Feedbck cancelado!")
+            else:
+                print("Digite uma opção válida!")
+        elif fbopcao == 5:
+            fbduvidas = input("\nEscreva seu feedback: ")
+            print("\nTem certeza que vai enviar o feedback? \n1 - Sim \n2 - Não")
+            fbopcao2 = int(input("\nSelecione uma opção: "))
+            if fbopcao2 == 1:
+                print("Feedback enviado!")
+            elif fbopcao2 == 2:
+                print("Feedbck cancelado!")
+            else:
+                print("Digite uma opção válida!")
+        else:
+             print("\nDigite uma opção válida!")
 
-
-          
 
