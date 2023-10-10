d82cf72d512882ecd3f63a5c540b75af5a3efe13
rafaellabastos1 <rm552425@fiap.com.br>
2023-10-10 11:37:38 -0300

    arrumei as aspas do programa

--- challenge.py
+++ challenge.py
@@ -10,14 +10,14 @@ def Confirmacao():
 #Case 1: mostrar os tipos de seguro
 def TipoSeguro():
         print('\nEssas são as opções de seguro disponibilizadas pela nossa empresa: '
-                    + "\n1- Para ciclistas que pedalam na rua"
-                    + "\n2- Para ciclistas de maratona"
-                    + "\n3- Para ciclistas que pedalam em montanhas"
-                    + "\n4- Para ciclistas que pedalam em pedras e rochas"
-                    + "\n5- Para ciclistas que pedalam em terra e mato"
-                    + "\n6- Para ciclistas por hobbie"
-                    + "\n7- Para ciclistas que viajam com a bike"
-                    + "\n--------------------------------------------")
+                    + '\n1- Para ciclistas que pedalam na rua'
+                    + '\n2- Para ciclistas de maratona'
+                    + '\n3- Para ciclistas que pedalam em montanhas'
+                    + '\n4- Para ciclistas que pedalam em pedras e rochas'
+                    + '\n5- Para ciclistas que pedalam em terra e mato'
+                    + '\n6- Para ciclistas por hobbie'
+                    + '\n7- Para ciclistas que viajam com a bike'
+                    + '\n--------------------------------------------')
         
 
 #Case 2: para identificar o cliente no sistema
@@ -25,32 +25,32 @@ def IdentificarCliente():
     opcRegistro = 2
 
     while opcRegistro == 2:
-        cpf = input("\nDigite seu CPF: ")
-        opcRegistro = int(input(f"\nO CPF {cpf} está correto?"
-                                + "\n1 - Sim"
-                                + "\n2 - Não"
-                                + "\nEscolha uma opção: "))
+        cpf = input('\nDigite seu CPF: ')
+        opcRegistro = int(input(f'\nO CPF {cpf} está correto?'
+                                + '\n1 - Sim'
+                                + '\n2 - Não'
+                                + '\nEscolha uma opção: '))
         if opcRegistro == 1:
             tuplaCPF = (cpf)
-            print(f"\nO CPF {tuplaCPF} foi encontrado. Seja bem vindo!")
+            print(f'\nO CPF {tuplaCPF} foi encontrado. Seja bem vindo!')
 
 
 #Case 2: para escolher um tipo de seguro        
 def RegistroSeguro():
     confirma = 2
     while confirma != 1:
-        print("\nEssas são as opções de seguro disponibilizadas pela nossa empresa: "
-                    + "\n1- Para ciclistas que pedalam na rua"
-                    + "\n2- Para ciclistas de maratona"
-                    + "\n3- Para ciclistas que pedalam em montanhas"
-                    + "\n4- Para ciclistas que pedalam em pedras e rochas"
-                    + "\n5- Para ciclistas que pedalam em terra e mato"
-                    + "\n6- Para ciclistas por hobbie"
-                    + "\n7- Para ciclistas que viajam com a bike")
+        print('\nEssas são as opções de seguro disponibilizadas pela nossa empresa: '
+                    + '\n1- Para ciclistas que pedalam na rua'
+                    + '\n2- Para ciclistas de maratona'
+                    + '\n3- Para ciclistas que pedalam em montanhas'
+                    + '\n4- Para ciclistas que pedalam em pedras e rochas'
+                    + '\n5- Para ciclistas que pedalam em terra e mato'
+                    + '\n6- Para ciclistas por hobbie'
+                    + '\n7- Para ciclistas que viajam com a bike')
         try:
             seguro = int(input('\nQual dessas opções combina mais com seu estilo? '))
             if seguro < 1 or seguro > 7:
-                print("\nDigite um número válido!")
+                print('\nDigite um número válido!')
             confirma = Confirmacao()
 
             if confirma == 1:
@@ -66,117 +66,117 @@ def RegistroSeguro():
 
 #Case 2: para enviar as fotos e vídeos para a vistoria
 def MidiaVistoria():
-        print("\nClique no x para adicionar a foto: ")
+        print('\nClique no x para adicionar a foto: ')
         
-        print("\n x- Foto da bike inteira de lado")
+        print('\n x- Foto da bike inteira de lado')
         confirmFoto = Confirmacao()
         while confirmFoto == 2:
-            print("Repita o processo")
-            print("\n x- Foto da bike inteira de lado")
+            print('Repita o processo')
+            print('\n x- Foto da bike inteira de lado')
             confirmFoto = Confirmacao()
         if confirmFoto == 1:
-            print("\nFoto adicionada.")
+            print('\nFoto adicionada.')
             
-        print("\n x- Foto do número de série")
+        print('\n x- Foto do número de série')
         confirmFoto = Confirmacao()
         while confirmFoto == 2:
-            print("Repita o processo")
-            print("\n x - Foto do número de série")
+            print('Repita o processo')
+            print('\n x - Foto do número de série')
             confirmFoto = Confirmacao()
         if confirmFoto == 1:
-            print("\nFoto adicionada")
+            print('\nFoto adicionada')
         
-        print("\n x- Foto da roda")
+        print('\n x- Foto da roda')
         confirmFoto = Confirmacao()
         while confirmFoto == 2:
-            print("Repita o processo")
-            print("\n x- Foto da roda")
+            print('Repita o processo')
+            print('\n x- Foto da roda')
             confirmFoto = Confirmacao()
         if confirmFoto == 1:
-            print("\nFoto adicionada.")
+            print('\nFoto adicionada.')
 
-        print("\n x- Foto dos freios")
+        print('\n x- Foto dos freios')
         confirmFoto = Confirmacao()
         while confirmFoto == 2: 
-            print("Repita o processo")
-            print("\n x- Foto dos freios")
+            print('Repita o processo')
+            print('\n x- Foto dos freios')
             confirmFoto = Confirmacao()
         if confirmFoto == 1:
-            print("\nFoto adicionada.")
+            print('\nFoto adicionada.')
         
-        print("\n x- Foto do guidão")
+        print('\n x- Foto do guidão')
         confirmFoto = Confirmacao()
         while confirmFoto == 2:
-            print("Repita o processo")
-            print("\n x- Foto do guidão")
+            print('Repita o processo')
+            print('\n x- Foto do guidão')
             confirmFoto = Confirmacao()
         if confirmFoto == 1:
-            print("\nFoto adicionada.")
+            print('\nFoto adicionada.')
 
-        print("\n x- Foto dos pedais")
+        print('\n x- Foto dos pedais')
         confirmFoto = Confirmacao()
         while confirmFoto == 2:
-            print("Repita o processo")
-            print("\n x- Foto dos pedais")
+            print('Repita o processo')
+            print('\n x- Foto dos pedais')
             confirmFoto = Confirmacao()
         if confirmFoto == 1:
-            print("\nFoto adicionada.")
+            print('\nFoto adicionada.')
         
-        print("\n x- Foto da corrente")
+        print('\n x- Foto da corrente')
         confirmFoto = Confirmacao()
         while confirmFoto == 2:
-            print("Repita o processo")
-            print("\n x- Foto da corrente")
+            print('Repita o processo')
+            print('\n x- Foto da corrente')
             confirmFoto = Confirmacao()
         if confirmFoto == 1:
-            print("\nFoto adicionada.")
+            print('\nFoto adicionada.')
 
-        print("\n x- Foto sua com a bike")
+        print('\n x- Foto sua com a bike')
         confirmFoto = Confirmacao()
         while confirmFoto == 2:
-            print("Repita o processo")
-            print("\n x- Foto sua com a bike")
+            print('Repita o processo')
+            print('\n x- Foto sua com a bike')
             confirmFoto = Confirmacao()
         if confirmFoto == 1:
-            print("\nFoto adicionada.")
+            print('\nFoto adicionada.')
 
-        print("\n x- Foto da bike de frente")
+        print('\n x- Foto da bike de frente')
         confirmFoto = Confirmacao()
         while confirmFoto == 2:
-            print("Repita o processo")
-            print("\n x- Foto da bike de frente")
+            print('Repita o processo')
+            print('\n x- Foto da bike de frente')
             confirmFoto = Confirmacao()
         if confirmFoto == 1:
-            print("\nFoto adicionada.")
+            print('\nFoto adicionada.')
 
-        print("\n x- Foto dos acessórios (se for visível)")
+        print('\n x- Foto dos acessórios (se for visível)')
         confirmFoto = Confirmacao()
         while confirmFoto == 2:
-            print("Repita o processo")
-            print("\n x- Foto dos acessórios (se for visível)")
+            print('Repita o processo')
+            print('\n x- Foto dos acessórios (se for visível)')
             confirmFoto = Confirmacao()
         if confirmFoto == 1:
-            print("\nFoto adicionada.")
+            print('\nFoto adicionada.')
 
-        print("\nClique no x para adicionar os vídeos: ")
+        print('\nClique no x para adicionar os vídeos: ')
 
-        print("\nx -Vídeo mostrando a bike completa")
+        print('\nx -Vídeo mostrando a bike completa')
         confirmFoto = Confirmacao()
         while confirmFoto == 2: 
-            print("Repita o processo")
-            print("\nx -Vídeo mostrando a bike completa")
+            print('Repita o processo')
+            print('\nx -Vídeo mostrando a bike completa')
             confirmFoto = Confirmacao()
         if confirmFoto == 1:
-            print("\nVídeo adicionado.")
+            print('\nVídeo adicionado.')
 
-        print("\nx -Vídeo mostrando com mais ênfase cada ponto chave que foi tirado foto")
+        print('\nx -Vídeo mostrando com mais ênfase cada ponto chave que foi tirado foto')
         confirmFoto = Confirmacao()
         while confirmFoto == 2:
-            print("Repita o processo")
-            print("\nx -Vídeo mostrando com mais ênfase cada ponto chave que foi tirado foto")
+            print('Repita o processo')
+            print('\nx -Vídeo mostrando com mais ênfase cada ponto chave que foi tirado foto')
             confirmFoto = Confirmacao()
         if confirmFoto == 1:
-            print("\nVídeo adicionado.")
+            print('\nVídeo adicionado.')
         
 
 #Case 2: iniciar o processo de vistoria
@@ -190,44 +190,44 @@ def Vistoria():
 
     RegistroSeguro()
 
-    print("\n-------------------------------------------")
-    print("Para finalizar a vistoria é necessário que sejam tiradas: "  
-            + "\n-Foto da bike inteira de lado"
-            + "\n-Foto do número de série"
-            + "\n-Foto da roda"
-            + "\n-Foto dos freios"
-            + "\n-Foto do guidão"
-            + "\n-Foto dos pedais"
-            + "\n-Foto da corrente"
-            + "\n-Foto sua com a bike"
-            + "\n-Foto da bike de frente"
-            + "\n-Foto dos acessórios (se for visível)"
-            + "\n-Vídeo mostrando a bike completa"
-            + "\n-Vídeo mostrando com mais ênfase cada ponto chave que foi tirado foto")
-    print("\nObservação: neste momento, como ainda não é possível enviar fotos e vídeos, esta parte não é totalmente funcional")
+    print('\n-------------------------------------------')
+    print('Para finalizar a vistoria é necessário que sejam tiradas: '
+            + '\n-Foto da bike inteira de lado'
+            + '\n-Foto do número de série'
+            + '\n-Foto da roda'
+            + '\n-Foto dos freios'
+            + '\n-Foto do guidão'
+            + '\n-Foto dos pedais'
+            + '\n-Foto da corrente'
+            + '\n-Foto sua com a bike'
+            + '\n-Foto da bike de frente'
+            + '\n-Foto dos acessórios (se for visível)'
+            + '\n-Vídeo mostrando a bike completa'
+            + '\n-Vídeo mostrando com mais ênfase cada ponto chave que foi tirado foto')
+    print('\nObservação: neste momento, como ainda não é possível enviar fotos e vídeos, esta parte não é totalmente funcional')
     
-    print("-----------------------------------")
+    print('-----------------------------------')
     MidiaVistoria()
     
-    print("\nOs seus dados foram enviados para vistoria. Você pode acompanhar o atual status da análise pelo seu e-mail ou aqui pelo site.")
+    print('\nOs seus dados foram enviados para vistoria. Você pode acompanhar o atual status da análise pelo seu e-mail ou aqui pelo site.')
     
-    print("------------------------------")
-    print("\nDeseja conferir o status da análise da vistoria?")
-    confirmVistoria = int(input("\n1 - Sim"
-                              + "\n2 - Não"
-                              + "\nSelecione uma opção: "))
+    print('------------------------------')
+    print('\nDeseja conferir o status da análise da vistoria?')
+    confirmVistoria = int(input('\n1 - Sim'
+                              + '\n2 - Não'
+                              + '\nSelecione uma opção: '))
 
     if confirmVistoria == 1: 
         if aprovado == True:
-            print("\nSeus dados foram aprovados! Agora assine a apólice.")
+            print('\nSeus dados foram aprovados! Agora assine a apólice.')
         elif reprovado == True:
-            print("\nSeus dados foram reprovados. Refaça o processo de vistoria.")
+            print('\nSeus dados foram reprovados. Refaça o processo de vistoria.')
         elif emAnalise == True:
-            print("\nSeus dados estão em análise.")
+            print('\nSeus dados estão em análise.')
         elif faltandoDocs == True:
-            print("\nEstá faltando documentos para realizar a vistoria. Revise seus dados.")
+            print('\nEstá faltando documentos para realizar a vistoria. Revise seus dados.')
     else:
-        print("Opção incorreta")
+        print('Opção incorreta')
 
 #Case 3: informa o status da vistoria
 def Status():
@@ -238,20 +238,20 @@ def Status():
 
     IdentificarCliente()
 
-    print("\nOs seus dados foram enviados para vistoria. Você pode acompanhar o atual status da análise pelo seu email ou aqui pelo site.")
-    print("\nDeseja conferir o status da análise da vistoria?")
-    confirmVistoria = int(input("\n1 - Sim"
-                              + "\n2 - Não" 
-                              + "\nSelecione uma opção: "))
+    print('\nOs seus dados foram enviados para vistoria. Você pode acompanhar o atual status da análise pelo seu email ou aqui pelo site.')
+    print('\nDeseja conferir o status da análise da vistoria?')
+    confirmVistoria = int(input('\n1 - Sim'
+                              + '\n2 - Não' 
+                              + '\nSelecione uma opção: '))
     if confirmVistoria == 1: 
             if aprovado == True:
-                print("Seus dados foram aprovados! Agora assine a apólice.")
+                print('Seus dados foram aprovados! Agora assine a apólice.')
             elif reprovado == True:
-                print("Seus dados foram reprovados. Refaça o processo de vistoria.")
+                print('Seus dados foram reprovados. Refaça o processo de vistoria.')
             elif emAnalise == True:
-                print("\nSeus dados estão em análise.")
+                print('\nSeus dados estão em análise.')
             elif faltandoDocs == True:
-                print("Está faltando documentos para realizar a vistoria. Revise seus dados.")
+                print('Está faltando documentos para realizar a vistoria. Revise seus dados.')
     else:
         print("\nOk. Você pode acompanhar no seu email ou voltar aqui para conferir o atual status da sua vistoria.")
 
@@ -262,27 +262,27 @@ def Status():
 def Nota():
     nota = 0
     while nota != -1:
-        nota = int(input("Qual a sua nota para esse serviço? (0 - 10): "))
+        nota = int(input('Qual a sua nota para esse serviço? (0 - 10): '))
         if nota < 0 or nota > 10:
-            print("\nNota inválida, tente novamente.")
+            print('\nNota inválida, tente novamente.')
             nota = 1
         else:
             tuplaNota = (nota)
             nota = -1
-            return f"Nota {tuplaNota} gravada com sucesso!" 
+            return f'Nota {tuplaNota} gravada com sucesso!'
            
     
 #Menu de opções
 opcaomenu = 0
 
 while opcaomenu != 5:
-    print("\nOlá, em que a Technobike pode teseguro para a bike")
-    print("1 - Tipo de seguro")
-    print("2 - Iniciar processo de vistoria")
-    print("3 - Conferir status da vistoria")
-    print("4 - Feedback")
-    print("5 - Encerrar programa")
-    opcaomenu = int(input("\nSelecione uma das opções acima: "))
+    print('\nOlá, em que a Technobike pode teseguro para a bike')
+    print('1 - Tipo de seguro')
+    print('2 - Iniciar processo de vistoria')
+    print('3 - Conferir status da vistoria')
+    print('4 - Feedback')
+    print('5 - Encerrar programa')
+    opcaomenu = int(input('\nSelecione uma das opções acima: '))
     
     match opcaomenu:
 #Tipos de seguro
@@ -303,22 +303,22 @@ while opcaomenu != 5:
 # Feedback
         case 4:
             opcaofb = [
-                {"motivo": "--Tempo para fazer vistoria", "nota": None},
-                {"motivo": "--Serviços fornecido", "nota": None},
-                {"motivo": "--Problemas", "nota": None},
-                {"motivo": "--Atendimento", "nota": None},
-                {"motivo": "--Resolução de dúvidas", "nota": None}
+                {'motivo': '--Tempo para fazer vistoria', 'nota': None},
+                {'motivo': '--Serviços fornecido', 'nota': None},
+                {'motivo': '--Problemas', 'nota': None},
+                {'motivo': '--Atendimento', 'nota': None},
+                {'motivo': '--Resolução de dúvidas', 'nota': None}
 ]           
 
             for feedback in opcaofb:
-                motivo = feedback["motivo"]
-                print(f"\n{motivo}")
-                feedback["nota"] = Nota()
-                print("Feedback enviado")
+                motivo = feedback['motivo']
+                print(f'\n{motivo}')
+                feedback['nota'] = Nota()
+                print('Feedback enviado')
                 print(f"{feedback['nota']}")
 
 
 ## Encerrar o programa
         case 5:
-            print("\nFim de programa, até a próxima!")
+            print('\nFim de programa, até a próxima!')
             break            
